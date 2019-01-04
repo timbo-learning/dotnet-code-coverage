6c17a7dec98f647d58cf4a8c3ff2034840970739
Rafael Timbó <timbo.rafa@gmail.com>
2019-01-04 13:32:00 -0500

    report_generator verbosity

--- CalculationTests/report_generator.py
+++ CalculationTests/report_generator.py
@@ -3,6 +3,9 @@ import os
 #import configargparse
 import argparse
 
+headerSign = "=" * 10
+
+
 def parse_arguments(raw_args):
     calculationXml = os.path.join('..', 'calculation.opencover.xml')
     primeXml = os.path.join('..', 'prime.opencover.xml')
@@ -18,6 +21,10 @@ def parse_arguments(raw_args):
     parser.add_argument('-rt', '--reporttypes',
         default=os.getenv('reporttypes',
             'HTML;HTMLChart;XML;PngChart;Badges'))
+    parser.add_argument('-v', '--verbosity',
+        type=int, default=3)
+    parser.add_argument('-q', '--quiet',
+        dest='verbosity', action='store_const', const=0)
 
     #print(parser.format_values())
     return parser.parse_args(raw_args)
@@ -34,9 +41,13 @@ def reportgeneratorargs(args):
 def main(raw_args=None):
     reportgenerator = "reportgenerator"
     args = parse_arguments(raw_args)
+    
+    args.verbosity >= 3 and print("\nINFO: " + headerSign + "Report Generator" + headerSign + "\n")
+
     cmd = "dotnet " + reportgenerator + reportgeneratorargs(args)
     print(cmd)
     os.system(cmd)
+    args.verbosity >= 3 and print("INFO: " + headerSign + "End of Report Generator" + headerSign)
 
 if __name__ == '__main__':
     main()
