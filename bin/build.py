2aab73d61088a974ff1e6b8efbad522dcd6cf25a
Rafael Timbó <timbo.rafa@gmail.com>
2019-01-16 16:10:04 -0500

    simplified args, passing extra args to children

--- bin/build.py
+++ bin/build.py
@@ -3,7 +3,9 @@ import os
 import sys
 import shutil
 import argparse
-import test_and_report
+import config
+import coverlet
+import report_generator
 
 def parse_arguments(raw_args):
     parser = argparse.ArgumentParser()
@@ -21,13 +23,14 @@ def parse_arguments(raw_args):
         )
     parser.add_argument('-v', '--version')
     parser.add_argument('-d', '--define',
-        nargs='*')
+        action='append')
+    #parser.add_argument('args', nargs=argparse.REMAINDER)
     parser.add_argument('--test',
         dest='test', action='store_true')
     parser.add_argument('--no-test',
         dest='test', action='store_false')
-    parser.set_defaults(test=False)
-    return parser.parse_args(raw_args)
+    parser.set_defaults(test=True)
+    return parser.parse_known_args(raw_args)
 
 def sonar_args(args):
 
@@ -44,8 +47,9 @@ def sonar_args(args):
         xml=' /s:"' + cwd + args.sonar_xml + '"'
 
     defined_variables=""
-    for parameter in args.define:
-        defined_variables+=' /d:' + parameter
+    
+    if (args.define):
+        defined_variables=[' /d:' + parameter for parameter in args.define]
 
     return key + xml + defined_variables
 
@@ -53,7 +57,35 @@ def sonar_cmd(args):
 
     return '"' + args.sonar_scanner + '"'
 
-def build(args):
+def coverlet_call(target, folder, output, child_args):
+    print("child_args:", child_args)
+    arglist = [
+        '--testfolder', folder,
+        '--target', target,
+        '--format', config.outputFormat,
+        #'--output', config.primeOutput,
+        '--format', 'lcov',
+        #'--output', 'prime.lcov.info',
+        '--no-test'
+        ]
+    arglist.extend(child_args)
+    print('coverlet.main ', arglist)
+    coverlet.main(arglist)
+    os.rename(coverlet.DEFAULT_OPENCOVER_OUTPUT, output)
+    #Update lcov.info to see line coverage on VS Code
+    os.rename(coverlet.DEFAULT_LCOV_OUTPUT, os.path.join(folder, 'lcov.info'))
+
+def test(child_args):
+    os.system("dotnet test")
+
+    coverlet_call(target=config.primeTarget, folder=config.primeFolder, output=config.primeOutput, child_args=child_args)
+    coverlet_call(target=config.calculationTarget, folder=config.calculationFolder, output=config.calculationOutput, child_args=child_args)
+
+    #lcov.main()
+    #Generate the report using Report Generator
+    report_generator.main([])
+
+def build(args, child_args):
     sonarqubeXml="SonarQube.Analysis.xml"
     defaultSonarqubeXml=os.path.join("bin",".store","dotnet-sonarscanner","4.5.0","dotnet-sonarscanner", "4.5.0", "tools", "netcoreapp2.1", "any",
             sonarqubeXml)
@@ -70,15 +102,16 @@ def build(args):
     os.system( sonar_full_cmd )
 
     os.system("dotnet build")
-    if (not args.test):
-        sys.argv = [sys.argv[0]]
-        test_and_report.main()
+    if (args.test):
+        test(child_args)
+        #sys.argv = [sys.argv[0]]
+        #test_and_report.main()
         #os.system("python " + os.path.join("bin","test_and_report.py"))
     os.system(sonarscanner +  ' end')
 
 def main(raw_args=None):
-    args = parse_arguments(raw_args)
-    build(args)
+    args, child_args = parse_arguments(raw_args)
+    build(args, child_args)
 
 if __name__ == '__main__':
     main()
--- bin/coverlet.py
+++ bin/coverlet.py
@@ -4,6 +4,9 @@ import argparse
 import config
 import logger
 
+DEFAULT_OPENCOVER_OUTPUT='coverage.opencover.xml'
+DEFAULT_LCOV_OUTPUT     ='coverage.info'
+
 def validate_args(args):
     if not args.testfolder:
         raise ValueError("--testfolder required")
@@ -15,46 +18,46 @@ def parse_arguments(raw_args):
     parser.add_argument('-tf', '--testfolder',
         default=os.getenv('testfolder', config.calculationFolder )
         )
-    parser.add_argument('-f', '--format',
-        default=os.getenv('format', 'opencover')
-        )
-    parser.add_argument('-o', '--output',
-        default=os.getenv('output', 'output.opencover.xml')
-        )
     parser.add_argument('-t', '--target',
         default=os.getenv('target', config.calculationTarget )
         )
+    #parser.add_argument('-f', '--format',
+    #    action='append'
+    #    )
+    #parser.add_argument('-o', '--output',
+    #    default=os.getenv('output', 'output.opencover.xml')
+    #    )
     parser.add_argument('--test', dest='test', action='store_true')
     parser.add_argument('--no-test', dest='test', action='store_false')
     parser.set_defaults(test=True)
-    return parser.parse_args(raw_args)
+    return parser.parse_known_args(raw_args)
 
 def coverlet(args):
-    print(args)
     coverletPath = os.path.join('bin', 'coverlet')
+    print(args)
     cmd = "%(coverlet)s %(target)s" \
             + ' --exclude "[xunit.runner.*]*" ' \
-            + ' --target dotnet --targetargs "test %(testfolder)s --no-build" ' \
-            + ' -o %(output)s' \
-            + ' --format %(format)s' \
+            + ' --target dotnet --targetargs "test %(testfolder)s --no-build" '
+            #+ ' -o %(output)s' \
+            #+ ' --format %(format)s' \
             # lcov to show lines on Visual Studio Code
     cmd = cmd % {
         'coverlet': coverletPath,
-        'target': args.target,
         'testfolder': args.testfolder,
-        'output': args.output,
-        'format': args.format
-        } 
-    print(">>>>" + cmd)
+        'target': args.target
+    }
     return cmd
 
 def main(raw_args=None):
     log = logger.Logger(name='coverlet', format='level')
     log.infoTitle()
-    args = parse_arguments(raw_args)
+    args, child_args = parse_arguments(raw_args)
     validate_args(args)
     cmd = coverlet(args)
+    print("child_args:",child_args)
+    cmd += " " + " ".join(child_args)
     args.test and os.system("dotnet test")
+    print(">>>>" + cmd + " ")
     os.system(cmd)
     log.infoEndTitle()
 
